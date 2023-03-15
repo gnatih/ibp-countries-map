d21c1dae68cb2f840aee82617810e07d088317a7
Jayesh Gohel <jayesh.gohel@gmail.com>
2023-03-15 23:36:16 +0530

    📦️ v1

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,97 @@
 <script setup>
-import * as d3 from 'd3'
-
+import * as d3 from "d3";
+import * as topojson from "topojson";
 import { onMounted } from "vue";
-
-
-const data = [
-  { date: "24-Apr-07", amount: 93.24 },
-  { date: "25-Apr-07", amount: 95.35 },
-  { date: "26-Apr-07", amount: 98.84 },
-  { date: "27-Apr-07", amount: 99.92 },
-  { date: "30-Apr-07", amount: 99.8 },
-  { date: "1-May-07", amount: 99.47 },
-  { date: "2-May-07", amount: 100.39 },
-  { date: "3-May-07", amount: 100.4 },
-  { date: "4-May-07", amount: 100.81 },
-  { date: "7-May-07", amount: 103.92 },
-  { date: "8-May-07", amount: 105.06 },
-  { date: "9-May-07", amount: 106.88 },
-  { date: "10-May-07", amount: 107.34 },
-];
-const parseTime = d3.timeParse("%d-%b-%y");
-const width = 800;
-const height = 500;
-let projection = d3.geoEquirectangular();
-let geoGenerator = d3.geoPath()
-  .projection(projection);
+import _ from "lodash";
 
 onMounted(() => {
+  Promise.all([
+    d3.json("countries-50m.json"),
+    d3.json(
+      "https://opensheet.elk.sh/15Fhb7nWSG0WlKzlD96Qy8VfjHuZPa4P0AVIKJqALPtM/countries_db"
+    ),
+  ]).then(([c, d]) => {
+    // console.log(c.objects.countries, d);
 
-  d3.json('world-110m.json').then(countries => {
-    console.log(countries)
-  })
+    _.remove(
+      c.objects.countries.geometries,
+      (c3) => c3.properties.name == "Antarctica"
+    );
 
+    const width = document.body.clientWidth;
+    const height = document.body.clientHeight;
+    let svg = d3.select("svg");
+    let g = svg.append("g");
+    let projection = d3
+      .geoNaturalEarth1()
+      .translate([width / 2, height / 2])
+      .scale((width - 1) / 2 / Math.PI);
+    let geoGenerator = d3.geoPath().projection(projection);
 
-  const svg = d3.select("svg").attr("width", width).attr("height", height);
-  const g = svg.append("g");
-  const x = d3
-    .scaleTime()
-    .domain(
-      d3.extent(data, function (d) {
-        return parseTime(d.date);
+    let colors = {
+      "OBS Country": "#52C3C9",
+      "Multiple Projects": "#0083A9",
+      "Country Office": "#034A8A",
+    };
+
+    svg = d3.select("svg").attr("width", width).attr("height", height);
+    g.selectAll("path")
+      .data(topojson.feature(c, c.objects.countries).features)
+      .enter()
+      .append("path")
+      .attr("d", geoGenerator)
+      .attr("fill", function (country) {
+        let c = _.find(d, { Country: country.properties.name });
+        return !_.isUndefined(c) ? colors[c.Type] : "#ccc";
       })
-    )
-    .rangeRound([0, width]);
-  const y = d3
-    .scaleLinear()
-    .domain(
-      d3.extent(data, function (d) {
-        return d.amount;
-      })
-    )
-    .rangeRound([height, 0]);
-  const line = d3
-    .line()
-    .x(function (d) {
-      return x(parseTime(d.date));
-    })
-    .y(function (d) {
-      return y(d.amount);
-    });
+      .attr("class", (country) => _.kebabCase(country.properties.name))
+      .attr("stroke-width", 1)
+      .attr("stroke", "#fff");
 
-  g.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
+    let zoom = d3
+      .zoom()
+      .scaleExtent([1, 8])
+      .on("zoom", (e) => {
+        g.selectAll("path")
+          .attr("transform", e.transform)
+          .attr("stroke-width", 1 / e.transform.k);
+      });
+
+    svg.call(zoom);
+  });
+
+  // const x = d3
+  //   .scaleTime()
+  //   .domain(
+  //     d3.extent(data, function (d) {
+  //       return parseTime(d.date);
+  //     })
+  //   )
+  //   .rangeRound([0, width]);
+  // const y = d3
+  //   .scaleLinear()
+  //   .domain(
+  //     d3.extent(data, function (d) {
+  //       return d.amount;
+  //     })
+  //   )
+  //   .rangeRound([height, 0]);
+  // const line = d3
+  //   .line()
+  //   .x(function (d) {
+  //     return x(parseTime(d.date));
+  //   })
+  //   .y(function (d) {
+  //     return y(d.amount);
+  //   });
+
+  // g.append("path")
+  //   .datum(data)
+  //   .attr("fill", "none")
+  //   .attr("stroke", "steelblue")
+  //   .attr("stroke-width", 1.5)
+  //   .attr("d", line);
 });
-
 </script>
 
 <template>
@@ -75,4 +99,7 @@ onMounted(() => {
 </template>
 
 <style scoped>
+svg {
+  outline: 1px solid #ccc;
+}
 </style>
